7e20dfd897c40447e0205e86e387b92edc6abac5
janncarlos <janncarlosmacahiya@gmail.com>
2023-05-21 13:37:30 +0800

    center to do app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import {AiOutlinePlus} from 'react-icons/ai'
+import {FaPlus} from 'react-icons/fa'
 import Todo from './Todo';
 import {db} from './firebase'
 import {
@@ -14,11 +14,11 @@ import {
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#008080] to-[#C0C0C0]`,
-  container: `bg-slate-100 max-w-[500px] w-full n-auto rounded-md shadow-xl p-4`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
   heading: `text.3xl font-bold text-center text-gray-800 p-2`,
   form: `flex justify-between`,
   input: `border p-2 w-full text-xl`,
-  button: `border p-4 ml-2 bg-silver-500 text-slate-100`,
+  button: `border p-4 ml-2 bg-silver-500 `,
   count: `text-center p-2`
 };
 
@@ -69,7 +69,7 @@ const deleteTodo = async (id) => {
 }
 
   return (
-    <div className={style.bg}>
+    <div className={`${style.bg}`}>
       <div className={style.container}>
         <h3 className={style.heading}>Todo App</h3>
         <form onSubmit={createTodo} className={style.form}>
@@ -80,7 +80,7 @@ const deleteTodo = async (id) => {
           type="text" 
           placeholder='Type Here' 
           />
-          <button type="button" className={style.button}><AiOutlinePlus size={50} /></button>
+          <button type="button" onClick={(e)=> createTodo(e)} className={style.button}><FaPlus /></button>
         </form>
         <ul>
           {todos.map((todo, index)=>
